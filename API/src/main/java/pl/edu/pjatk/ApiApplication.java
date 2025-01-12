a55885458422bca78312555075257d9b7c6c6579
Snizhana <s29870@pjwstk.edu.pl>
2025-01-13 00:53:11 +0100

    initial commit

---
+++ API/src/main/java/pl/edu/pjatk/ApiApplication.java
@@ -0,0 +1,10 @@
+package pl.edu.pjatk;
+
+import org.springframework.boot.SpringApplication;
+
+@org.springframework.boot.autoconfigure.SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
