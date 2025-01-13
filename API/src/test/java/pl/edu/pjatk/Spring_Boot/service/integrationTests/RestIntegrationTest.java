3e986ea13cb378174834d2e9e529de39c0f62885
s29870 <s29870@pjwstk.edu.pl>
2025-01-13 15:50:49 +0100

    add integration tests for all endpoints in API module

--- API/src/test/java/pl/edu/pjatk/Spring_Boot/service/integrationTests/RestIntegrationTest.java
+++ API/src/test/java/pl/edu/pjatk/Spring_Boot/service/integrationTests/RestIntegrationTest.java
@@ -28,7 +28,7 @@ public class RestIntegrationTest {
         RestAssured.get(basePath + "/car/all")
                 .then()
                 .statusCode(200)
-                .body("$.size()", is(10));
+                .body("$.size()", is(5));
     }
 
     @Test
@@ -37,6 +37,47 @@ public class RestIntegrationTest {
                 .body(new Car("Ford", "Midnight Blue"))
                 .header("Content-Type", "application/json")
                 .post(basePath + "/car")
+                .then()
+                .statusCode(201);
+    }
+
+    @Test
+    public void getCarReturnsCarById() {
+        RestAssured.get(basePath + "/car/100")
+                .then()
+                .statusCode(200)
+                .body("id", is(100));
+    }
+
+    @Test
+    public void getCarReturnsCarByBrand() {
+        RestAssured.get(basePath + "/brand/Tesla")
+                .then()
+                .statusCode(200)
+                .body("[0].brand", is("Tesla"));
+    }
+
+    @Test
+    public void getCarReturnsCarByColor() {
+        RestAssured.get(basePath + "/color/White")
+                .then()
+                .statusCode(200)
+                .body("[0].color", is("White"));
+    }
+
+    @Test
+    public void putCarUpdatesCarsFields() {
+        RestAssured.with()
+                .body(new Car("Tesla", "Midnight Blue"))
+                .header("Content-Type", "application/json")
+                .put(basePath + "/update/100")
+                .then()
+                .statusCode(201);
+    }
+
+    @Test
+    public void deleteCarDeletesCar() {
+        RestAssured.delete(basePath + "/delete/100")
                 .then()
                 .statusCode(201);
     }
