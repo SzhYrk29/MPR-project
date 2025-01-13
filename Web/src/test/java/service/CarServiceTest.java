5a520fb26284a0ba0879e16265f776f3b1b6ea9e
s29870 <s29870@pjwstk.edu.pl>
2025-01-13 15:49:18 +0100

    add a few new tests in Web module for RestClient

--- Web/src/test/java/service/CarServiceTest.java
+++ Web/src/test/java/service/CarServiceTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.web.client.MockServerRestClientCustomizer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.client.match.MockRestRequestMatchers;
@@ -19,6 +20,7 @@ import pl.edu.pjatk.service.CarService;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 
 @RestClientTest(CarService.class)
 @ContextConfiguration(classes = Config.class)
@@ -83,4 +85,76 @@ public class CarServiceTest {
 
         assertEquals(expectedCar.getId(), actualCar.getId());
     }
+
+    @Test
+    public void shouldReturnCarsByBrand() {
+        String responseBody = """
+                [
+                    {"id": 1, "brand": "Tesla", "color": "White", "identifier": 12345},
+                    {"id": 2, "brand": "Tesla", "color": "Black", "identifier": 34567}
+                ]
+                """;
+
+        customizer.getServer()
+                .expect(MockRestRequestMatchers
+                        .requestTo("brand/Tesla"))
+                .andRespond(MockRestResponseCreators.withSuccess(responseBody, MediaType.APPLICATION_JSON));
+
+        List<Car> actualCars = service.getCarByBrand("Tesla");
+
+        List<Car> expectedCars = List.of(
+                new Car(1L, "Tesla", "White", 12345),
+                new Car(2L, "Tesla", "Black", 34567)
+        );
+
+        assertEquals(expectedCars.getFirst().getBrand(), actualCars.getFirst().getBrand());
+    }
+
+    @Test
+    public void shouldReturnCarsByColor() {
+        String responseBody = """
+                [
+                    {"id": 1, "brand": "Tesla", "color": "Black", "identifier": 12345},
+                    {"id": 2, "brand": "BMW", "color": "Black", "identifier": 34567}
+                ]
+                """;
+
+        customizer.getServer()
+                .expect(MockRestRequestMatchers
+                        .requestTo("color/Black"))
+                .andRespond(MockRestResponseCreators.withSuccess(responseBody, MediaType.APPLICATION_JSON));
+
+        List<Car> actualCars = service.getCarByColor("Black");
+
+        List<Car> expectedCars = List.of(
+                new Car(1L, "Tesla", "Black", 12345),
+                new Car(2L, "BMW", "Black", 34567)
+        );
+
+        assertEquals(expectedCars.getFirst().getColor(), actualCars.getFirst().getColor());
+    }
+
+//    @Test
+//    public void shouldAddCar() {
+//        Car carToAdd = new Car(1L, "BMW", "Black", 12345);
+//
+//        String responseBody = """
+//                {
+//                    "id": 1, "brand": "BMW", "color": "Black", "identifier": 12345
+//                }
+//                """;
+//
+//        customizer.getServer()
+//                .expect(MockRestRequestMatchers
+//                        .requestTo("car"))
+//                .andExpect(MockRestRequestMatchers.method(HttpMethod.POST))
+//                .andExpect(MockRestRequestMatchers.content().json("{\"brand\":\"BMW\",\"color\":\"Black\",\"identifier\":12345}"))
+//                .andRespond(MockRestResponseCreators.withSuccess(responseBody, MediaType.APPLICATION_JSON));
+//
+//        service.addCar(carToAdd);
+//
+//        Car exprectedCar = new Car(1L, "BMW", "Black", 12345);
+//
+//        assertEquals(carToAdd, service.getCarById(exprectedCar.getId()));
+//    }
 }
