e43c894eedd1ecf688a898acc9a808d97ed05c76
Snizhana <s29870@pjwstk.edu.pl>
2025-01-22 23:23:29 +0100

    add CarNotFoundException in endpoints in RestController

--- API/src/main/java/pl/edu/pjatk/controller/MyRestController.java
+++ API/src/main/java/pl/edu/pjatk/controller/MyRestController.java
@@ -1,17 +1,16 @@
 package pl.edu.pjatk.controller;
 
-import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import pl.edu.pjatk.exception.CarNotFoundException;
 import pl.edu.pjatk.exception.InvalidInputException;
 import pl.edu.pjatk.model.Car;
 import pl.edu.pjatk.service.CarService;
 
-import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -27,15 +26,17 @@ public class MyRestController {
 
     @GetMapping("car/all") // <- endpoint (displays all cars)
     public ResponseEntity<Iterable<Car>> getAll() {
-        logger.info("Endpoint \"get all\" was invoked.");
+        logger.info("Endpoint \"getCarById all\" was invoked.");
         return new ResponseEntity<>(this.carService.getCars(), HttpStatus.OK);
     }
 
     @GetMapping("car/{id}") // <- endpoint (gets a car by id)
-    public ResponseEntity<Car> get(@PathVariable String id) {
+    public ResponseEntity<Car> getCarById(@PathVariable String id) {
         try {
             Long parsedId = Long.parseLong(id);
-            logger.info("Endpoint \"get by id\" was invoked using GET http method with id \"{}\".", id);
+            if (parsedId <= 0)
+                throw new CarNotFoundException();
+            logger.info("Endpoint \"getCarById by id\" was invoked using GET http method with id \"{}\".", id);
             return new ResponseEntity<>(this.carService.getCarById(parsedId), HttpStatus.OK);
         } catch (NumberFormatException e) {
             throw new InvalidInputException();
@@ -54,7 +55,7 @@ public class MyRestController {
         try {
             Long parsedId = Long.parseLong(id);
             if (parsedId <= 0)
-                throw new InvalidInputException();
+                throw new CarNotFoundException();
             this.carService.deleteCar(parsedId);
             logger.info("Endpoint \"delete by id\" was invoked using DELETE http method with id \"{}\".", id);
             return new ResponseEntity<>(HttpStatus.CREATED);
@@ -68,7 +69,7 @@ public class MyRestController {
         try {
             Long parsedId = Long.parseLong(id);
             if (parsedId <= 0)
-                throw new InvalidInputException();
+                throw new CarNotFoundException();
             this.carService.updateCar(parsedId, car);
             logger.info("Endpoint \"update by id\" was invoked using GET http method with data: id \"{}\", brand=\"{}\", color=\"{}\".", car.getId(), car.getBrand(), car.getColor());
             return new ResponseEntity<>(HttpStatus.CREATED);
@@ -80,26 +81,26 @@ public class MyRestController {
 
     @GetMapping("brand/{brand}") // <- endpoint (gets a car by brand)
     public ResponseEntity<List<Car>> getByBrand(@PathVariable String brand) {
-        logger.info("Endpoint \"get by brand\" was invoked using GET http method with brand \"{}\".", brand);
+        logger.info("Endpoint \"getCarById by brand\" was invoked using GET http method with brand \"{}\".", brand);
         return new ResponseEntity<>(this.carService.getCarByBrand(brand), HttpStatus.OK);
     }
 
     @GetMapping("color/{color}") // <- endpoint (gets a car by color)
     public ResponseEntity<List<Car>> getByColor(@PathVariable String color) {
-        logger.info("Endpoint \"get by color\" was invoked using GET http method with color \"{}\".", color);
+        logger.info("Endpoint \"getCarById by color\" was invoked using GET http method with color \"{}\".", color);
         return new ResponseEntity<>(this.carService.getCarByColor(color), HttpStatus.OK);
     }
 
-    @GetMapping("pdf/{id}") // <- endpoint (gets pdf with info about car with given id)
-    public ResponseEntity<Void> getPdf(@PathVariable String id, HttpServletResponse response) throws IOException {
-        try {
-            Long parsedId = Long.parseLong(id);
-            this.carService.getPdf(parsedId, response);
-            logger.info("Endpoint \"get pdf\" was invoked using GET http method.");
-            return new ResponseEntity<>(HttpStatus.OK);
-        } catch (NumberFormatException e) {
-            throw new InvalidInputException();
-        }
-
-    }
+//    @GetMapping("pdf/{id}") // <- endpoint (gets pdf with info about car with given id)
+//    public ResponseEntity<Void> getPdf(@PathVariable String id, HttpServletResponse response) throws IOException {
+//        try {
+//            Long parsedId = Long.parseLong(id);
+//            this.carService.getPdf(parsedId, response);
+//            logger.info("Endpoint \"getCarById pdf\" was invoked using GET http method.");
+//            return new ResponseEntity<>(HttpStatus.OK);
+//        } catch (NumberFormatException e) {
+//            throw new InvalidInputException();
+//        }
+//
+//    }
 }
